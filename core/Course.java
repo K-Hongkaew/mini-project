0028280884443040075f03631a4a4806be96d68d
Petch Hongkaew <82324764+K-Hongkaew@users.noreply.github.com>
2025-09-27 05:51:20 +0700

    refactor(grade): นำ enrollments ออกแล้วนำมารวมกับ Class grade
    - ลดความยุ่งยากของการทำงาน
    - คำนวนคะแนนในตัวเอง

--- core/Course.java
+++ core/Course.java
@@ -2,7 +2,7 @@ package core;
 
 import java.util.ArrayList;
 
-class Course {
+public class Course {
     private final int MAX_SCORE = 100; // กันคะแนนเกิน
 
     // คะแนน
@@ -10,23 +10,102 @@ class Course {
     private int MAX_FINAL;
     private int MAX_ASSIGNMENT; // Assignments Score
 
+    private String grade = "F";// start at F
+
+    private int midtermScore;
+    private int finalScore;
+    private int assignmentScore;
+    private int currentScore;
+
     private String courseID; // เก็บเป็น String จะทำให้อ่านง่ายกว่า
     private String courseName;
-    private Professor assignedProfessor; // เอาไว้หา เจ้าตัว
     private ArrayList<Student> studentInCourse;
+    private Professor professorInCourse;
+    private Department onDepartment; // course อยู่ใน department ไหน
 
-    // TODO: function getter setter ค่าต่างๆ ของคะแนน
-    // เมื่อ student ลง enrollment เอา student ลงใน studentInCourse
-    // print ชื่อนิสิตทุกคนใน Course พร้อมชื่อ อาจารย์ที่สอน
-
-    public Course(String CourseID, String CourseName, Professor assignProfessor, int MID, int FINAL, int ASSIGN) {
+    public Course(String CourseID, String CourseName, int MID, int FINAL, int ASSIGN, Professor professor,
+            Department department) {
         this.courseID = CourseID;
         this.courseName = CourseName;
-        this.assignedProfessor = assignProfessor;
         this.MAX_MIDTERM = MID;
         this.MAX_FINAL = FINAL;
         this.MAX_ASSIGNMENT = ASSIGN;
-        this.studentInCourse = new ArrayList<>(); 
+        this.studentInCourse = new ArrayList<>();
+        this.professorInCourse = professor;
+        this.onDepartment = department;
+        professorInCourse.addCourse(this);
+        onDepartment.addCourseToDepartment(this);
+        this.midtermScore = 0;
+        this.finalScore = 0;
+        this.assignmentScore = 0;
+        this.currentScore = 0;
+    }
+
+    // --------------- GETTER SETTER ---------------
+    public void setMidtermScore(int score) {
+        this.midtermScore = score;
+    }
+
+    public void setFinalScore(int score) {
+        this.finalScore = score;
+    }
+
+    public void setAssignmentScore(int score) {
+        this.assignmentScore = score;
+    }
+
+    public String getCourseGrade() {
+        return this.grade;
+    }
+
+    public int getMidtermScore() {
+        return this.midtermScore;
+    }
+
+    public int getFinalScore() {
+        return this.finalScore;
+    }
+
+    public int getAssignmentScore() {
+        return this.assignmentScore;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professorInCourse = professor;
+    }
+
+    public void overallScore() {
+        System.out.println("If score over is more than 100 it will be cut off.");
+        this.currentScore += assignmentScore;
+        this.currentScore += midtermScore;
+        this.currentScore += finalScore;
+        if (this.currentScore > MAX_SCORE) {
+            this.currentScore = MAX_SCORE;
+        }
+        gradeCalculation(this.currentScore);
+    }
+
+    private void gradeCalculation(int score) {
+        if (score >= 80)
+            this.grade = "A";
+        else if (score >= 75)
+            this.grade = "B+";
+        else if (score >= 70)
+            this.grade = "B";
+        else if (score >= 65)
+            this.grade = "C+";
+        else if (score >= 60)
+            this.grade = "C";
+        else if (score >= 55)
+            this.grade = "D+";
+        else if (score >= 50)
+            this.grade = "D";
+        else
+            this.grade = "F";
+    }
+
+    public int getCurrentScore() {
+        return this.currentScore;
     }
 
     public void setCourseID(String CourseID) {
@@ -45,14 +124,6 @@ class Course {
     public String getCourseName() {
         return this.courseName;
 
-    }
-
-    public void setAssignedProfessor(Professor assignProfessor) {
-        this.assignedProfessor = assignProfessor;
-    }
-
-    public Professor getAssignedProfessor() {
-        return this.assignedProfessor;
     }
 
     public void setMAX_MIDTERM(int MID) {
@@ -86,11 +157,18 @@ class Course {
     }
 
     public void enrollStudent(Student student) {
-        studentInCourse.add(student);
+        if (!studentInCourse.contains(student)) {
+            studentInCourse.add(student);
+            student.addCourse(this);
+        }
     }
 
     public void removeStudent(Student student) {
         studentInCourse.remove(student);
+    }
+
+    public Professor getProfessor() {
+        return this.professorInCourse;
     }
 
     public ArrayList<Student> getStudentInCourse() {
@@ -98,10 +176,6 @@ class Course {
     }
 
     public void printCourseInfo() {
-        System.out.println("Course: " + courseID + "  " + courseName);
-        if (assignedProfessor != null) {
-            System.out.println("Professor: " + assignedProfessor.getName());
-        }
         if (studentInCourse.isEmpty()) {
             System.out.println("No students enrolled.");
         } else {
